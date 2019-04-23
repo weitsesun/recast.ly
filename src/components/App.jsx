1913e87425e1c71805dc029a15c04dc1e7db9ba8
weitsesun <wtse.sun@gmail.com>
2019-04-22 20:30:26 -0700

    Implement VideoList, VideoListEntry and VideoPlayer

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,25 @@
-var App = () => (
-  <div>
+import VideoList from './VideoList.js';
+import exampleVideoData from "../data/exampleVideoData.js"
+import VideoPlayer from './VideoPlayer.js';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentVideo: ""
+    }
+  }
+
+  onClickVideo() {
+    this.setState({
+      currentVideo: this
+    });
+    console.log(this.state.currentVideo)
+  }
+
+  render() {
+    return (<div>
     <nav className="navbar">
       <div className="col-md-6 offset-md-3">
         <div><h5><em>search</em> view goes here</h5></div>
@@ -7,14 +27,25 @@ var App = () => (
     </nav>
     <div className="row">
       <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
+        <div>
+          <h5>
+            <em>videoPlayer</em> 
+            <VideoPlayer video={exampleVideoData[0]}/>
+          </h5>
+          </div>
       </div>
       <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
+        <div>
+          <h5>
+            <em>videoList</em> 
+            <VideoList videos={exampleVideoData} onClick={this.onClickVideo.bind(this)}/>
+        </h5>
+        </div>
       </div>
     </div>
-  </div>
-);
+  </div>);
+  }
+}
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,10 +1,9 @@
-var VideoList = () => (
+//{exampleVideoData}
+import VideoListEntry from "./VideoListEntry.js"
+
+var VideoList = (props) => (
   <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    {props.videos.map(elem => <VideoListEntry video={elem}/>)}  
   </div>
 );
 
